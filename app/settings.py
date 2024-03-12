dd1b626ba2bd7356bed6ec284593e180a207a0f2
maksim-nevekin <maksim.new99@gmail.com>
2024-03-12 18:09:36 +0300

    Setting up the display of discounts

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +45,6 @@ INSTALLED_APPS = [
 
     "main",
     "goods",
-    
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,11 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 INTERNAL_IPS = [
     # ...
