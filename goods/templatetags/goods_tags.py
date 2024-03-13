f91db2bf18d6da69072ddab6154d5fb6d5279cf8
maksim-nevekin <maksim.new99@gmail.com>
2024-03-13 15:48:53 +0300

    The product filter on the site page and urlunicode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from django.utils.http import urlencode
 from goods.models import Categories
 from django import template
 
@@ -8,3 +9,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
