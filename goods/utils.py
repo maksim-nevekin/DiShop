18c24bb7609726eabb4ee7dbfd316bbfb498a026
maksim-nevekin <maksim.new99@gmail.com>
2024-03-13 19:00:18 +0300

    Set up a site search

--- goods/utils.py
+++ goods/utils.py
@@ -1,15 +1,42 @@
-from re import search
 from goods.models import Products
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 
 def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
+
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        headline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
     
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    return result
