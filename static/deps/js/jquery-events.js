f9c1e1e2036d6cad08cd8c2f7e5f0514e4fec801
maksim-nevekin <maksim.new99@gmail.com>
2024-03-14 15:25:11 +0300

    Setup profile form controller and interactive notification on pages

--- static/deps/js/jquery-events.js
+++ static/deps/js/jquery-events.js
@@ -2,11 +2,11 @@
 $(document).ready(function () {
     // Берем из разметки элемент по id - оповещения от django
     var notification = $('#notification');
-    // И через 7 сек. убираем
+    // И через 4 сек. убираем
     if (notification.length > 0) {
         setTimeout(function () {
             notification.alert('close');
-        }, 7000);
+        }, 4000);
     }
 
     // При клике по значку корзины открываем всплывающее(модальное) окно
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from django.contrib import auth
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
+
 
 
 def login(request):
@@ -15,6 +17,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f'Вы успешно вошли в аккаунт {username}')
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -32,6 +35,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f'Регистрация успешно завершена, Вы вошли в аккаунт {user.username}')
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -42,12 +46,25 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Изменения обновлены')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         'title': 'Home - Кабинет',
+        'form': form,
     }
     return render(request, 'users/profile.html', context)
 
 def logout(request):
+    messages.success(request, f'Вы вышли из аккаунта {request.user.username}')
     auth.logout(request)
+    
     return redirect(reverse('main:index'))
