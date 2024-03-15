68ac14724d4de6f65c30650e14fb690d3c7b6d34
maksim-nevekin <maksim.new99@gmail.com>
2024-03-15 12:20:30 +0300

    Setup admin panel

--- carts/models.py
+++ carts/models.py
@@ -31,5 +31,11 @@ class Cart(models.Model):
         return round(self.product.sell_price() * self.quantity, 2)
 
     def __str__(self) -> str:
-        return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
+        if self.user:
+            return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
+        
+        return f'Зарегистрируйтесь или войдите в аккаунт для оформления заказа | Товар {self.product.name} | Количество {self.quantity}'
+
+
+        
     
