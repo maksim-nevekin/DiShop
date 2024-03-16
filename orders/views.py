5c9d76a61bca667be68492f70a17cbd9a3eda7cb
maksim-nevekin <maksim.new99@gmail.com>
2024-03-16 12:10:08 +0300

    Create custom phone number validator

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,3 @@
-from email import message
 from django.contrib import messages
 from django.db import transaction
 from django.forms import ValidationError
@@ -10,22 +9,22 @@ from orders.models import Order, OrderItem
 
 
 def create_order(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateOrderForm(data=request.POST)
         if form.is_valid():
             try:
                 with transaction.atomic():
                     user = request.user
                     cart_items = Cart.objects.filter(user=user)
-                    
+
                     if cart_items.exists():
                         # Создать заказ
                         order = Order.objects.create(
                             user=user,
-                            phone_number=form.cleaned_data['phone_number'],
-                            requires_delivery=form.cleaned_data['requires_delivery'],
-                            delivery_address=form.cleaned_data['delivery_address'],
-                            payment_on_get=form.cleaned_data['payment_on_get'],
+                            phone_number=form.cleaned_data["phone_number"],
+                            requires_delivery=form.cleaned_data["requires_delivery"],
+                            delivery_address=form.cleaned_data["delivery_address"],
+                            payment_on_get=form.cleaned_data["payment_on_get"],
                         )
                         # Создать заказанные товары
                         for cart_item in cart_items:
@@ -35,9 +34,11 @@ def create_order(request):
                             quantity = cart_item.quantity
 
                             if product.quantity < quantity:
-                                raise ValidationError(f'Недостаточное количество товара {name} на складе\
-                                                      В наличии {product.quantity}')
-                            
+                                raise ValidationError(
+                                    f"Недостаточное количество товара {name} на складе\
+                                                       В наличии - {product.quantity}"
+                                )
+
                             OrderItem.objects.create(
                                 order=order,
                                 product=product,
@@ -47,28 +48,25 @@ def create_order(request):
                             )
                             product.quantity -= quantity
                             product.save()
-                    
-                    # Очистить корзину пользователя после создания заказа
-                    cart_items.delete()
 
-                    messages.success(request, 'Заказ оформлен')
-                    return redirect('user:profile')
+                        # Очистить корзину пользователя после создания заказа
+                        cart_items.delete()
 
+                        messages.success(request, "Заказ оформлен!")
+                        return redirect("user:profile")
             except ValidationError as e:
                 messages.success(request, str(e))
-                return redirect('cart:order')
-
+                return redirect("cart:order")
     else:
         initial = {
-            'first_name': request.user.first_name,
-            'last_name': request.user.last_name,
+            "first_name": request.user.first_name,
+            "last_name": request.user.last_name,
         }
 
         form = CreateOrderForm(initial=initial)
 
     context = {
-        'title': 'HOME - Оформление заказа',
-        'form': form,
+        "title": "Home - Оформление заказа",
+        "form": form,
     }
-
-    return render(request, 'orders/create_order.html', context=context)
+    return render(request, "orders/create_order.html", context=context)
